815fe87b1ed0292adcd416056a85441a8be9f715
Jessica Daye <jddaye@hotmail.com>
2022-04-06 20:03:44 -0400

    fix typo

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
-import { GiftedChat } from 'react-native-gifted-chat';
-import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { Platform, KeyboardAvoidingView } from 'react-native';
 
 export default class Chat extends React.Component {
   
@@ -20,7 +20,7 @@ export default class Chat extends React.Component {
         {
           _id: 1,
           text: 'Hello developer',
-          craetedAt: new Date(),
+          createdAt: new Date(),
           user: {
             _id: 2,
             name: 'React Native',
@@ -30,7 +30,7 @@ export default class Chat extends React.Component {
         {
           _id: 2,
           text: 'This is a system message',
-          craetedAt: new Date(),
+          createdAt: new Date(),
           system: true,
         },
       ],
@@ -52,6 +52,9 @@ export default class Chat extends React.Component {
         wrapperStyle={{
           right: {
             backgroundColor: '#000'
+          },
+          left: {
+            backgroundColor: '#333'
           }
         }}
       />
@@ -64,16 +67,6 @@ export default class Chat extends React.Component {
     this.props.navigation.setOptions({ title: name});
 
     const { bgColor } = this.props.route.params;
-    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
-
-    <GiftedChat
-    renderBubble={this.renderBubble.bind(this)}
-    messages={this.state.messages}
-    onSend={messages => this.onSend(messages)}
-    user={{
-      _id: 1,
-    }}
-  />
 
     return (
       <TouchableOpacity
@@ -88,7 +81,16 @@ export default class Chat extends React.Component {
           alignItems: 'center',
           backgroundColor: bgColor
           }}>
+          <GiftedChat
+            renderBubble={this.renderBubble.bind(this)}
+            messages={this.state.messages}
+            onSend={messages => this.onSend(messages)}
+            user={{
+              _id: 1,
+            }}
+          />
           <Text>Hello Chat!</Text>
+          { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
         </View>
       </TouchableOpacity>
     )
