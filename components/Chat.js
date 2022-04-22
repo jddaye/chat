616aa85679b4facca90161de9529f576c31cce3b
Jessica Daye <jddaye@hotmail.com>
2022-04-22 05:27:00 -0400

    installed async

--- components/Chat.js
+++ components/Chat.js
@@ -3,7 +3,7 @@ import { View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import firebase from "firebase";
 import "firebase/firestore";
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import NetInfo from '@react-native-community/netinfo'
 
 const firebaseConfig = {
